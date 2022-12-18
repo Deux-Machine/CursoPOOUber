b16e23ff232fc7349bfffeec9a5b7f08e6b6d55a
Sebastian Alvarez <alvarezsebas552@gmail.com>
2022-12-18 16:31:49 -0500

    Se termina el constrcutor de Java y Python, Se deja inciado el Index de JavaScript

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,12 @@
+from Car import Car
+
 if __name__ == "__main__":
-    print("Hola Perrita")
+    car = Car()
+    car.license = "PHQ81F"
+    car.driver = "Freddy"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "CUCA69"
+    car2.driver = "El putas"
+    print(vars(car2))
