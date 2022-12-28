1e4b50f92dad2f5b0bc1fa34652b2e2069f782c9
Sebastian Alvarez <alvarezsebas552@gmail.com>
2022-12-27 20:13:53 -0500

    Se agregan las crean las clases User y Driver en el lenguaje JavaScript

--- Js/index.js
+++ Js/index.js
@@ -5,3 +5,6 @@ car.printDataCar();
 var uberX = new UberX("SST312", new Account("Sebas Alvarez", "AND345"), "Chevrolet", "Spark")
 uberX.passenger = 4;
 uberX.printDataCar();
+
+var User = new User("Damian Waines", "AJG643", "damian-waines@hotmail.com", "31248456");
+User.printDataUser();
